d4e8e1aa79d0e20156ebfa22ddf438a0763debdc
makecodefun22 <makecodefun22@gmail.com>
2024-10-02 00:16:36 +0530

    S

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           {/* Logo */}
-          <Image width={250} height={200} src='/logo.png' className=""/>
+          <Image alt='LOGO' width={250} height={200} src='/logo.png'/>
 
           {/* Hamburger for mobile */}
           <div className="md:hidden">
