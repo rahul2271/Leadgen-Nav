a9f8ad36ceba925ff817ab38960612cfb6a44c7b
Rahul <121254290+rahul2271@users.noreply.github.com>
2024-10-12 10:17:58 +0530

    Update testimo.js

--- src/app/components/testimo.js
+++ src/app/components/testimo.js
@@ -128,7 +128,7 @@ const Testimonials = () => {
         {testimonials.map((testimonial) => (
           <div
             key={testimonial.id}
-            className="min-w-[400px] p-10 bg-white text-black rounded-lg shadow-lg hover:scale-105 transition-transform duration-300"
+            className="min-w-[300px] p-10 bg-white text-black rounded-lg shadow-lg hover:scale-105 transition-transform duration-300"
           >
             <p className="mb-2">{testimonial.text}</p>
             <h4 className="font-semibold">- {testimonial.author}</h4>
